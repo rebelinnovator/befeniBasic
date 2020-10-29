63b57482d55a9e3eeddf76d74fa762044eca4531
apolo <apollo423@protonmail.com>
2020-10-29 09:33:48 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	  <title>Befeni Test</title>
+</head>
+<body>
+	<?php include('server.php') ?>
+</body>
+</html>
---
+++ server.php
@@ -0,0 +1,5 @@
+<?php
+	$myfile = fopen("input.txt", "r") or die("Unable to open file!");
+	echo "Hello"
+?>
+
