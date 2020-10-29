974bdd1f71d442e37617210ae14e4eedcd2099a4
apolo <apollo423@protonmail.com>
2020-10-29 10:28:06 -0400

    last commit

--- index.php
+++ index.php
@@ -2,8 +2,30 @@
 <html>
 <head>
 	  <title>Befeni Test</title>
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">	 
+	 </script>
 </head>
 <body>
 	<?php include('server.php') ?>
+	<div class="container">
+		<div class="row mt-5 border-bottom">
+			<h1>Result</h1>
+		</div>
+ 		<div class="row pt-2 border">
+ 			<div class="col-6 border-right">
+ 				<?php
+ 					echo $problem;
+ 				?>
+ 			</div>
+ 			<div class="col-6">
+ 				<?php
+ 					echo $total;
+ 				?>
+ 			</div>
+      	
+      	</div>
+  </div>
 </body>
 </html>
--- server.php
+++ server.php
@@ -1,5 +1,51 @@
 <?php
-	$myfile = fopen("input.txt", "r") or die("Unable to open file!");
-	echo "Hello"
+	$myfile = fopen("input1.txt", "r") or die("Unable to open file!");
+
+	$operator = array();
+	$number = array();
+
+	while(!feof($myfile)) {
+		$line = fgets($myfile);
+		$opepos = strpos($line, " ");
+		$opstring = substr($line, 0,$opepos);
+		$opnumber = substr($line, $opepos,strlen($line));
+		if($opstring != ""){
+			array_push($operator, $opstring);
+			array_push($number,$opnumber);	
+		}
+	  	
+	}
+	fclose($myfile);
+
+	$problem = "";
+	for ($i=0; $i < count($operator); $i++) { 
+		$problem .= $operator[$i].":".$number[$i]."<br>";
+	}
+
+	$total = intval($number[0]);
+	for ($i=0; $i < count($operator); $i++) { 
+		if($i != count($operator) - 1)
+			$num = intval($number[$i + 1]);
+		switch ($operator[$i]) {
+			case 'add':
+				$total += $num;
+				break;
+			case 'multiply':
+				$total *= $num;
+				break;
+			case 'subtract':
+				$total -= $num;
+				break;
+			case 'divide':
+				$total /= $num;
+				break;
+			case 'apply':
+				$total = $total;
+				break;
+			default:
+				# code...
+				break;
+		}
+	}
 ?>
 
